15e035400911ecf634fb45ab66066c595619446c
shockstove <2247157972@qq.com>
2023-11-27 23:08:46 +0800

    11.27

--- test2/test2.cpp
+++ test2/test2.cpp
@@ -1,11 +1,33 @@
 #include<iostream>
-
+#include<string>
 using namespace std;
 
-int main()
+int main( )
 {
-	char a;
-	cin>>a;
-	int b = (int)((unsigned char)a<<8);
-	return 0;
+    char a[10006];
+    cin>>a;
+    int k;
+    cin>>k;
+    int maxNum = 0;
+    bool b[10006]={0};
+    int i=0;
+    for(;a[i]!='\0';i++)
+    {
+        if(a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u'){
+            b[i]=1;
+        }
+        else b[i]=0;
+    }
+    for(int m=0;m<i-k-1;m++)
+    {
+        int temp=0;
+        for(int j=m;j<k+m;j++){
+            temp+=b[j];
+        }
+        if(temp>maxNum){
+            maxNum=temp;
+        }
+    }
+    cout<<maxNum<<endl;
+    return 0;
 }
